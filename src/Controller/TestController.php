cb8959d5bdb8a871f02d74b2165c05bcc4efc6dd
Maksym Haiduk <maksym.haiduk@gmail.com>
2023-08-06 14:40:54 +0300

    Install Redis client

--- src/Controller/TestController.php
+++ src/Controller/TestController.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace App\Controller;
 
+use Redis;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -12,13 +13,15 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 class TestController extends AbstractController
 {
 
-    public function __construct()
+    public function __construct(private readonly Redis $redisClient)
     {
     }
 
     #[Route('/test', name: 'test')]
     public function new(Request $request): Response
     {
+//        $this->redisClient->get();
+
         return new Response();
     }
 }
